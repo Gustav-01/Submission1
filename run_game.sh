5addd00a3ea34b55467fb202953771eea3887394
Gustav O <ac75309@skola.goteborg.se>
2025-10-19 18:23:04 +0200

    Added bash-script in run_game.sh

---
+++ run_game.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DIRECTORY="Gustav_Olsson_labb"
+
+echo "Gustav Olsson"
+mkdir "$DIRECTORY"
+cp *.java "$DIRECTORY"
+cd "$DIRECTORY"
+echo "Currently in: $(pwd)"
+echo "Compiling..."
+javac "GuessingGame.java"
+echo "Running game..."
+java "GuessingGame"
+echo "Done"
+rm *.class
+ls
+
